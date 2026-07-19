a3c279cb04127c2f7965ddbc6aac4d7fc7e93673
mollemolle <takeshi.pdt@gmail.com>
2026-07-19 12:07:00 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,13 @@ git switch -c my-v25.12.5
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-# 3. 設定ファイルの取得と反映
+# 3. 【超重要：lzoエラーを完全回避する対策】
+# どんな文字パターンで書かれていても、liblzoの組み立て命令を設計図から完全に消去します
+sed -i 's/tools-y += liblzo//g' ./tools/Makefile
+sed -i 's/tools-$(CONFIG_MTD_STATIC_LZO) += liblzo//g' ./tools/Makefile
+sed -i 's/tools-$(CONFIG_TARGET_orion_generic) += liblzo//g' ./tools/Makefile
+
+# 4. 設定ファイルの取得と反映
 wget https://downloads.openwrt.org/releases/25.12.5/targets/mediatek/mt7622/config.buildinfo -O .config
 #echo "CONFIG_TARGET_MULTI_PROFILE=y" >> .config
 #echo "CONFIG_TARGET_DEVICE_mediatek_mt7622_DEVICE_buffalo_wsr-3200ax4s=y" >> .config
@@ -20,23 +26,23 @@ echo "# CONFIG_TARGET_MULTI_PROFILE is not set" >> .config
 echo "CONFIG_TARGET_mediatek_mt7622_DEVICE_buffalo_wsr-3200ax4s=y" >> .config
 
 
-# 4.【追加】コンパイルキャッシュ（ccache）の有効化設定
+# 5.【追加】コンパイルキャッシュ（ccache）の有効化設定
 echo "CONFIG_CCACHE=y" >> .config
 echo "CONFIG_CCACHE_DIR=\"$GITHUB_WORKSPACE/.ccache\"" >> .config
 make defconfig
 
-# 5. カスタムファイルの取得と設定の追加
+# 6. カスタムファイルの取得と設定の追加
 wget https://pastebin.com/raw/yQTBrDaA -O ./target/linux/mediatek/dts/mt7622-buffalo-wsr-3200ax4s.dts
 echo "CONFIG_MTD_VIRT_CONCAT=y" >> ./target/linux/mediatek/mt7622/config-6.12
 echo  5721f98a447ca737b75326f25e62c50c > ./vermagic
 
-# 6. ファイルの書き換え（viの手間を無くして全自動化）
+# 7. ファイルの書き換え（viの手間を無くして全自動化）
 set +e
 sed -i 's|grep '\''=\[ym\]'\'' \$(LINUX_DIR)/\.config\.set \| LC_ALL=C sort \| \$(MKHASH) md5 > \$(LINUX_DIR)/\.vermagic|cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic|' ./include/kernel-defaults.mk
 sed -i 's|STAMP_BUILT:=\$(STAMP_BUILT)_\$(shell \$(SCRIPT_DIR)/kconfig\.pl \$(LINUX_DIR)/\.config \| \$(MKHASH) md5)|STAMP_BUILT:=$(STAMP_BUILT)_$(shell cat $(LINUX_DIR)/.vermagic)|' ./package/kernel/linux/Makefile
 set -e
 
-# 7. ビルドの実行
+# 8. ビルドの実行
 make defconfig
 make -j$(nproc) world
 
